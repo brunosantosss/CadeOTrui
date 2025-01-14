f89a50f03fdd10139277cd930c53e75995243a0c
brunosantosss <85379055+brunosantosss@users.noreply.github.com>
2025-01-14 17:15:43 -0300

    up

---
+++ backend/public/script.js
@@ -0,0 +1,31 @@
+const serverURL = "https://192.168.18.131:3000/gps";
+
+function sendGPSLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const { latitude, longitude } = position.coords;
+                console.log(latitude, longitude);
+        
+                fetch(serverURL, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ latitude, longitude })
+                })
+                .then(response => console.log("Dados de GPS enviados: ", response))
+                .catch(error => console.log("Ocorreu algum erro ao enviar informações de GPS: ", error));
+            },
+            error => {
+                console.error("Erro ao obter a localização: ", error);
+                alert("Erro ao acessar a localização.");
+            }
+        );        
+    }
+    else {
+        alert("Seu dispositivio não possui suporte para o envio de geolocalização.");
+    }
+}
+
+document.getElementById("getLocationBtn").addEventListener("click", () => {
+    sendGPSLocation();
+});
