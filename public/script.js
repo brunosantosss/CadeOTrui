f615e2cdf260c0893f797e3b4ef9d397f78832ff
brunosantosss <85379055+brunosantosss@users.noreply.github.com>
2025-01-15 14:36:20 -0300

    (feat) alert para mostrar a lat e long

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ function sendGPSLocation() {
             position => {
                 const { latitude, longitude } = position.coords;
                 console.log(latitude, longitude);
-                alert("FOI3");
+                alert(`Suas coordenadas: ${latitude}, ${longitude}`);
         
                 fetch(serverURL, {
                     method: "POST",
